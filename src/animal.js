8d74d09673f685768a16ff39d430ded6ae313068
tshiamomahloko <tshiahloks@gmail.com>
2020-05-08 10:38:20 +0200

    Changed placed the animal Array inside home

--- src/animal.js
+++ src/animal.js
@@ -1,55 +1,37 @@
 class Animal {
-  setName(name) {
+  constructor(name) {
     this.name = name;
   }
+
   eat() {
     console.log(this.name + " eats");
   }
 }
+
 class Dog extends Animal {
   sound() {
     console.log("Dog barks");
   }
 }
-let dog = new Dog();
-dog.setName("Rax");
-dog.eat();
-dog.sound();
 
 class Cat extends Animal {
   sound() {
     console.log("Cat meow");
   }
 }
-let cat1 = new Cat();
-cat1.setName("Stormy");
-cat1.eat();
-cat1.sound();
 
-//IGNORE
-console.log("");
-console.log("*************************************");
-console.log("");
-//END_IGNORE
-
-let homeAnimals = [];
 class Home {
-  adoptPet(animal) {
-    homeAnimals.push(animal);
+  constructor(){
+    this.animalArray = [];
   }
+
+  adoptPet(animal) {
+    this.animalArray.push(animal);
+  }
+
   makeAllSounds() {
-    for (var increment = 0; increment < homeAnimals.length; increment++) {
-      homeAnimals[increment].sound();
+    for (var i = 0; i < this.animalArray.length; i++) {
+      this.animalArray[i].sound();
     }
   }
 }
-
-let home = new Home();
-let dog1 = new Dog();
-let dog2 = new Dog();
-let cat = new Cat();
-
-home.adoptPet(dog1);
-home.adoptPet(cat);
-home.adoptPet(dog2);
-home.makeAllSounds();
